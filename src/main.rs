df2a8cb3916bedce2a98da63bd5bb1a069e5c925
btwotwo <10519967+btwotwo@users.noreply.github.com>
2022-07-31 11:39:36 +0200

    Add get_task and waiting for the generation completion

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 use std::{fs};
 pub mod dalle;
-use futures::executor::block_on;
+use anyhow::Context;
+use futures::executor::{block_on, self};
 use serde_json::Value;
 use text_io::{scan, read};
 
-fn main() {
-    block_on(async_main()).unwrap()
-}
-
-async fn async_main() -> anyhow::Result<()>{
+#[tokio::main]
+async fn main() -> anyhow::Result<()>{
     let token = ger_token_from_config()?;
-    let dalle = dalle::Dalle::new(&token);
+    let dalle = dalle::Dalle::new(&token)?;
     println!("Enter prompt: ");
     let prompt: String = read!();
 
@@ -26,5 +24,7 @@ async fn async_main() -> anyhow::Result<()>{
 fn ger_token_from_config() -> anyhow::Result<String> {
     let config = fs::read_to_string("config.json")?;
     let config_json: Value = serde_json::from_str(&config)?;
-    Ok(config_json["token"].to_string())
+    let token = config_json["token"].as_str().context("token should be a valid str")?;
+
+    Ok(token.to_string())
 }
